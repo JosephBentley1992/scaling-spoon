b14131570d9193814e8d781672f6651177d30c6b
Joseph <bentleyj@mail.gvsu.edu>
2018-08-27 19:38:06 -0400

    Initial Commit

---
+++ Model/Bus/Robot.cs
@@ -0,0 +1,16 @@
+﻿namespace ScalingSpoon.Bus
+{
+    public class Robot
+    {
+        public int Id { get; set; }
+        public Robot()
+        {
+        }
+
+        public Robot(int id)
+        {
+            Id = id;
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScalingSpoon
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Console.WriteLine("Is this properly set as the startup project?");
+            Console.WriteLine("It looks like I'm used to quite a few Resharper commands.");
+
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+        }
+    }
+}
