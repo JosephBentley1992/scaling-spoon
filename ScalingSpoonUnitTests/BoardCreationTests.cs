59bfeaafe1547949e3fd4317e5c3eb563f599821
Joseph <bentleyj@mail.gvsu.edu>
2019-06-30 19:47:55 -0400

    Added remove cell wall tests.

--- ScalingSpoonUnitTests/BoardCreationTests.cs
+++ ScalingSpoonUnitTests/BoardCreationTests.cs
@@ -6,10 +6,9 @@ using ScalingSpoon.Model.Enums;
 namespace ScalingSpoonTests
 {
     [TestClass]
-    public class BoardCreationTests
+    public class CreateCellWallTests
     {
         private static Engine _model;
-        private static Robot _robot;
 
         [TestInitialize]
         public void Setup()
@@ -104,6 +103,116 @@ namespace ScalingSpoonTests
             Assert.AreEqual(_model.Board[1, 2].Walls, CellWalls.Left);
             Assert.AreEqual(_model.Board[2, 1].Walls, CellWalls.Up);
             Assert.AreEqual(_model.Board[1, 0].Walls, CellWalls.Right);
+        }
+    }
+
+    [TestClass]
+    public class RemoveCellWallTests
+    {
+        private static Engine _model;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            /* a a a
+             * b b b
+             * c c c
+             */
+            _model = new Engine();
+            _model.Board = new Cell[3, 3];
+
+            int id = 0;
+            for (int x = 0; x <= _model.Board.GetLength(0) - 1; x++)
+                for (int y = 0; y <= _model.Board.GetLength(1) - 1; y++)
+                    _model.Board[x, y] = new Cell(id++, CellWalls.None, x, y);
+        }
+
+        [TestMethod]
+        public void CanDeleteUpWall()
+        {
+            _model.CreateCellWall(_model.Board[1, 1], Direction.Up);
+            _model.RemoveCellWall(_model.Board[1, 1], Direction.Up);
+            Assert.AreEqual(_model.Board[1, 1].Walls, CellWalls.None);
+            Assert.AreEqual(_model.Board[0, 1].Walls, CellWalls.None);
+        }
+
+        [TestMethod]
+        public void CanDeleteDownWall()
+        {
+            _model.CreateCellWall(_model.Board[1, 1], Direction.Down);
+            _model.RemoveCellWall(_model.Board[1, 1], Direction.Down);
+            Assert.AreEqual(_model.Board[1, 1].Walls, CellWalls.None);
+            Assert.AreEqual(_model.Board[2, 1].Walls, CellWalls.None);
+        }
+
+        [TestMethod]
+        public void CanDeleteRightWall()
+        {
+            _model.CreateCellWall(_model.Board[1, 1], Direction.Right);
+            _model.RemoveCellWall(_model.Board[1, 1], Direction.Right);
+            Assert.AreEqual(_model.Board[1, 1].Walls, CellWalls.None);
+            Assert.AreEqual(_model.Board[1, 2].Walls, CellWalls.None);
+        }
+
+        [TestMethod]
+        public void CanDeleteLeftWall()
+        {
+            _model.CreateCellWall(_model.Board[1, 1], Direction.Left);
+            _model.RemoveCellWall(_model.Board[1, 1], Direction.Left);
+            Assert.AreEqual(_model.Board[1, 1].Walls, CellWalls.None);
+            Assert.AreEqual(_model.Board[1, 0].Walls, CellWalls.None);
+        }
+
+        [TestMethod]
+        public void CanDeleteUpAndRight()
+        {
+            _model.CreateCellWall(_model.Board[1, 1], Direction.Up, Direction.Right);
+            _model.RemoveCellWall(_model.Board[1, 1], Direction.Up, Direction.Right);
+            Assert.AreEqual(_model.Board[1, 1].Walls, CellWalls.None);
+            Assert.AreEqual(_model.Board[0, 1].Walls, CellWalls.None);
+            Assert.AreEqual(_model.Board[1, 2].Walls, CellWalls.None);
+        }
+
+        [TestMethod]
+        public void CanDeleteDownAndLeft()
+        {
+            _model.CreateCellWall(_model.Board[1, 1], Direction.Down, Direction.Left);
+            _model.RemoveCellWall(_model.Board[1, 1], Direction.Down, Direction.Left);
+            Assert.AreEqual(_model.Board[1, 1].Walls, CellWalls.None);
+            Assert.AreEqual(_model.Board[2, 1].Walls, CellWalls.None);
+            Assert.AreEqual(_model.Board[1, 0].Walls, CellWalls.None);
+        }
+
+        [TestMethod]
+        public void CanDeleteRightAndDown()
+        {
+            _model.CreateCellWall(_model.Board[1, 1], Direction.Right, Direction.Down);
+            _model.RemoveCellWall(_model.Board[1, 1], Direction.Right, Direction.Down);
+            Assert.AreEqual(_model.Board[1, 1].Walls, CellWalls.None);
+            Assert.AreEqual(_model.Board[1, 2].Walls, CellWalls.None);
+            Assert.AreEqual(_model.Board[2, 1].Walls, CellWalls.None);
+        }
+
+        [TestMethod]
+        public void CanDeleteLeftAndUp()
+        {
+            _model.CreateCellWall(_model.Board[1, 1], Direction.Left, Direction.Up);
+            _model.RemoveCellWall(_model.Board[1, 1], Direction.Left, Direction.Up);
+            Assert.AreEqual(_model.Board[1, 1].Walls, CellWalls.None);
+            Assert.AreEqual(_model.Board[1, 0].Walls, CellWalls.None);
+            Assert.AreEqual(_model.Board[0, 1].Walls, CellWalls.None);
+        }
+
+        [TestMethod]
+        public void CanDeleteAll()
+        {
+            _model.CreateCellWall(_model.Board[1, 1], Direction.Up, Direction.Right, Direction.Left, Direction.Down);
+            _model.RemoveCellWall(_model.Board[1, 1], Direction.Up, Direction.Right, Direction.Left, Direction.Down);
+            Assert.AreEqual(_model.Board[1, 1].Walls, CellWalls.None);
+            Assert.AreEqual(_model.Board[0, 1].Walls, CellWalls.None);
+            Assert.AreEqual(_model.Board[1, 2].Walls, CellWalls.None);
+            Assert.AreEqual(_model.Board[2, 1].Walls, CellWalls.None);
+            Assert.AreEqual(_model.Board[1, 0].Walls, CellWalls.None);
         }
     }
 }
